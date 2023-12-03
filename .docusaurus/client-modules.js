5a9a954e1f2e2cd9feb48983542287dfa400bf0d
Mariana Andrade <marianaandrade@ua.pt>
2023-12-03 01:15:19 +0000

    remove files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/marianaandrade/Desktop/mei-es/projeto/documentation/node_modules/infima/dist/css/default/default.css'),
-  require('/home/marianaandrade/Desktop/mei-es/projeto/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/marianaandrade/Desktop/mei-es/projeto/documentation/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/marianaandrade/Desktop/mei-es/projeto/documentation/src/css/custom.css'),
-];
