821c0c7a47b59a0047eac376278ac85a86803f56
Brums21 <75539238+Brums21@users.noreply.github.com>
2023-12-09 23:59:47 +0000

    small error fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
             items: [
               {
                 label: 'Documentation',
-                to: '/docs/introduction',
+                to: '/',
               },
             ],
           },
